bdea3d6b77d29485afaeda4d308731b1a0ec7f7a
Marcelo Santos <marcelegald2@outlook.com>
2023-02-14 10:22:30 -0300

    Exercícios Semana 07

--- semana-sete/src/app/galeria/galeria.component.ts
+++ semana-sete/src/app/galeria/galeria.component.ts
@@ -3,14 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
-  styleUrls: ['./galeria.component.scss']
+  styleUrls: ['./galeria.component.scss'],
 })
 export class GaleriaComponent {
-
   currentImage = 0;
 
+  imgArray = [
+    '/assets/jojo.jpg',
+    '/assets/csm.jpg',
+    '/assets/geass.jpg',
+    '/assets/kaguya.png',
+    '/assets/away.jpg',
+  ];
+
   nextImage() {
-    if (this.currentImage !== 4) {
+    if (this.currentImage !== this.imgArray.length - 1) {
       this.currentImage += 1;
     }
   }
@@ -21,6 +28,11 @@ export class GaleriaComponent {
     }
   }
 
-  firstImage()
+  goToFirstImage() {
+    this.currentImage = 0;
+  }
 
+  goToLastImage() {
+    this.currentImage = this.imgArray.length - 1;
+  }
 }

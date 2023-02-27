97aac540ff8f00e54d55e4af59a79a7a11f42b05
Marcelo Santos <marcelegald2@outlook.com>
2023-02-26 23:11:36 -0300

    Exercícios semana 08

--- star-wars/src/environments/environment.development.ts
+++ star-wars/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/personagens/',
+  API_URL: 'http://localhost:3000/personagens/',
 };
--- star-wars/src/services/star-wars/star-wars.service.ts
+++ star-wars/src/services/star-wars/star-wars.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { StarWarsCharacterModel } from '../../models/star-wars-character/star.wars.character.model';
 
 @Injectable({
@@ -13,26 +13,27 @@ export class StarWarsService {
   constructor(private http: HttpClient) { }
 
   public getCharacter(characterId: string): void {
-    this.currentCharacters = this.http.get<StarWarsCharacterModel>(environment.apiUrl + characterId)
+    this.currentCharacters = this
+      .http.get<StarWarsCharacterModel>(environment.API_URL + characterId)
       .pipe(map((x) => ([x])));
   }
 
   public getAllCharacters(): void {
-    this.currentCharacters = this.http.get<StarWarsCharacterModel[]>(environment.apiUrl);
+    this.currentCharacters = this.http.get<StarWarsCharacterModel[]>(environment.API_URL);
   }
 
   public addCharacter(character: StarWarsCharacterModel): Observable<Object> {
-    return this.http.post(environment.apiUrl, character);
+    return this.http.post(environment.API_URL, character);
   }
 
   public deleteCharacter(characterId: string): Observable<Object> {
-    return this.http.delete(environment.apiUrl + characterId);
+    return this.http.delete(environment.API_URL + characterId);
   }
 
   public updateCharacter(
     characterId: string,
     character: StarWarsCharacterModel,
   ): Observable<Object> {
-    return this.http.put(environment.apiUrl + characterId, character);
+    return this.http.put(environment.API_URL + characterId, character);
   }
 }

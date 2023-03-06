fb64304175e5dae62ea104bc3245614777098a05
Marcelo Santos <marcelegald2@outlook.com>
2023-03-06 09:50:37 -0300

    Exercício semana 09

--- netflix/src/app/app.module.ts
+++ netflix/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -27,6 +28,7 @@ import { ContentComponent } from './layouts/content/content.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

d0d3c19fb67214f166d648c603f80a7c58c87306
Marcelo Santos <marcelegald2@outlook.com>
2023-03-06 06:42:15 -0300

    Exercício semana 09

--- netflix/src/app/app-routing.module.ts
+++ netflix/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { FullComponent } from './layouts/full/full.component';
 import { SeriesComponent } from './pages/series/series.component';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     component: FullComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'series/:id', component: SeriesComponent },

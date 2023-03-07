b1a466166b5c9c009def55db3b3a4cf2c703b4db
Marcelo Santos <marcelegald2@outlook.com>
2023-03-07 12:28:27 -0300

    Exercício semana 09

--- netflix/src/app/components/card/card.component.ts
+++ netflix/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { IMovie } from '../../models/imovie';
 
 @Component({
@@ -8,4 +9,13 @@ import { IMovie } from '../../models/imovie';
 })
 export class CardComponent {
   @Input('movie') public movie!:IMovie;
+
+  constructor(private readonly _router: Router) {
+  }
+
+  public redirectToMovie(): void {
+    const { id } = this.movie;
+    // eslint-disable-next-line no-underscore-dangle
+    this._router.navigate([`/series/${id}`]);
+  }
 }

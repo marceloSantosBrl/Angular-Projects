d26ceb253d1ed0d36cecbc976d2a3c2cda2ccdd3
Marcelo Santos <marcelegald2@outlook.com>
2023-03-07 13:38:36 -0300

    Exercícios semana 10

--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { EmailToUserPipe } from './pipes/email-to-user.pipe';
-import {HttpClientModule} from "@angular/common/http";
 import { ContentComponent } from './layouts/content/content.component';
 import { FullComponent } from './layouts/full/full.component';
 
@@ -18,15 +19,16 @@ import { FullComponent } from './layouts/full/full.component';
     NotFoundComponent,
     EmailToUserPipe,
     ContentComponent,
-    FullComponent
+    FullComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- e-commerce/src/app/pages/login/login.component.ts
+++ e-commerce/src/app/pages/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public readonly userCredentials: FormGroup<any> = this._fb.group({
+    email: ['', Validators.required],
+    password: ['',
+      [Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^[0-9]*$/)]],
+  });
 
+  constructor(private readonly _fb: FormBuilder) {
+  }
 }

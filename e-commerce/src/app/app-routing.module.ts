b7ea7178817c22e0b3216fa9c39da917595d4da6
Marcelo Santos Passos <marcelegald2@outlook.com>
2023-03-09 11:16:26 -0300

    Exercícios semana 10

--- e-commerce/src/app/app-routing.module.ts
+++ e-commerce/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./pages/login/login.component";
-import {NotFoundComponent} from "./pages/not-found/not-found.component";
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FullComponent } from './layouts/full/full.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: '**', component: NotFoundComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
